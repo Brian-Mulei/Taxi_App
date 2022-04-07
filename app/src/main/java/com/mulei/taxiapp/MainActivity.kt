296159a2728113e7c8bc0ca98c8c5d959e059f41
Brian-Mulei <brianmulei0@gmail.com>
2022-04-07 19:45:16 +0300

    phone auth

--- app/src/main/java/com/mulei/taxiapp/MainActivity.kt
+++ app/src/main/java/com/mulei/taxiapp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.mulei.taxiapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun phoneauth(view: View) {
+        val intent = Intent(this, phoneAuth::class.java)
+        startActivity(intent)
+
     }
 }
--- app/src/main/java/com/mulei/taxiapp/SplashActivity.kt
+++ app/src/main/java/com/mulei/taxiapp/SplashActivity.kt
@@ -4,10 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.PersistableBundle
 import android.view.WindowManager
+import com.google.firebase.auth.FirebaseAuth
 
 @Suppress("DEPRECATION")
-
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
